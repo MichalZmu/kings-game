623bfff0288c20d2b1d5003ceca2ac137e7512a5
MichalZmu <michal.zmuda.mail@gmail.com>
2020-04-06 07:38:40 +0200

    card picker improvments

--- src/app/shared/components/card-picker/card-options.ts
+++ src/app/shared/components/card-picker/card-options.ts
@@ -1,4 +1,17 @@
-export class CardOptions {
+export const CardOptions = [
   // todo: create a new object with cards
-
-}
+  { id: 1, name: '2', type: 'hearts' },
+  { id: 2, name: '3', type: 'hearts' },
+  { id: 3, name: '4', type: 'hearts'},
+  { id: 4, name: '4', type: 'hearts'},
+  { id: 5, name: '5', type: 'hearts'},
+  { id: 6, name: '6', type: 'hearts'},
+  { id: 7, name: '7', type: 'hearts'},
+  { id: 8, name: '8', type: 'hearts'},
+  { id: 9, name: '9', type: 'hearts'},
+  { id: 10, name: '10', type: 'hearts'},
+  { id: 11, name: 'Jack', type: 'hearts'},
+  { id: 12, name: 'Queen', type: 'hearts'},
+  { id: 13, name: 'King', type: 'hearts'},
+  { id: 13, name: 'Ace', type: 'hearts'}
+];
--- src/app/shared/components/card-picker/card-picker.component.ts
+++ src/app/shared/components/card-picker/card-picker.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CardOptions} from './card-options';
 
 @Component({
   selector: 'app-card-picker',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-picker.component.scss']
 })
 export class CardPickerComponent implements OnInit {
+  cardOptions = CardOptions;
+  card: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  chooseRandomCard(): void {
+    // todo: deal when there is no more cards
+    let tempCardList = this.cardOptions;
+    const index = Math.floor(Math.random() * this.cardOptions.length);
+    this.card = tempCardList[index].name + ' ' + tempCardList[index].type;
+    console.log(this.card);
+    tempCardList.splice(index, 1);
+    console.log('tempCardList: ', tempCardList);
+  }
 }
